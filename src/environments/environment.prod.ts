7a47c1b744d38abf6d5b9845bd51ccd8de5c2dc5
tosincoker26 <tosincoker26@gmail.com>
2021-01-30 10:12:54 +0100

    Initial Setup for pages and routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+     apiKey: "AIzaSyBKtZN_EXz49gGtZsotvdhPvnXBBREeltQ",
+    authDomain: "organic-shop-69810.firebaseapp.com",
+    databaseURL: "https://organic-shop-69810.firebaseio.com",
+    projectId: "organic-shop-69810",
+    storageBucket: "organic-shop-69810.appspot.com",
+    messagingSenderId: "1050881838708",
+    appId: "1:1050881838708:web:e3bd69b452b138f8594b21",
+    measurementId: "G-HRZ3ZEMKYF"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+     apiKey: "AIzaSyBKtZN_EXz49gGtZsotvdhPvnXBBREeltQ",
+    authDomain: "organic-shop-69810.firebaseapp.com",
+    databaseURL: "https://organic-shop-69810.firebaseio.com",
+    projectId: "organic-shop-69810",
+    storageBucket: "organic-shop-69810.appspot.com",
+    messagingSenderId: "1050881838708",
+    appId: "1:1050881838708:web:e3bd69b452b138f8594b21",
+    measurementId: "G-HRZ3ZEMKYF"
+  }
 };
 
 /*
